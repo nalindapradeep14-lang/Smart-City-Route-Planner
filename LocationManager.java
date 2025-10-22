c732f958b5eaf5baecc8bbe90f84fe6bd6814347
rasangamadhushan <rmsrasanga@gmail.com>
2025-10-22 20:01:01 +0530

    Update LocationManager.java
    Implement LocationManager class to handle user interactions for managing locations and roads

--- LocationManager.java
+++ LocationManager.java
@@ -8,4 +8,36 @@ public class LocationManager {
         this.graph = graph;
         this.scanner = new Scanner(System.in);
     }
+
+    public void addLocation() {
+        System.out.print("Enter Location name: ");
+        String name = scanner.nextLine().trim();
+        graph.addLocation(name);
+    }
+
+    public void removeLocation() {
+        System.out.print("Enter Location name to remove: ");
+        String name = scanner.nextLine().trim();
+        graph.removeLocation(name);
+    }
+
+    public void addRoad() {
+        System.out.print("Enter first location: ");
+        String loc1 = scanner .nextLine().trim();
+        System.out.print("Enter Second location: ");
+        String loc2 = scanner .nextLine().trim();
+        graph.addRoad(loc1, loc2);
+    }
+
+    public void removeRoad() {
+        System.out.print("Enter first location: ");
+        String loc1 = scanner .nextLine().trim();
+        System.out.print("Enter Second location: ");
+        String loc2 = scanner .nextLine().trim();
+        graph.removeRoad(loc1, loc2);
+    }
+
+    public void showConnections() {
+        graph.displayConnections();
+    }
 }
