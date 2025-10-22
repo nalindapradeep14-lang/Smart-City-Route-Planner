fdf45bcea8577c2ffa9cb9db9dc7bb89a3f71894
Nalinda <nalindagunasekara42@gmail.com>
2025-10-22 14:50:03 +0530

    Update Graph.java
    Add Graph Class With cityMap Structure

--- Graph.java
+++ Graph.java
@@ -1,8 +1,10 @@
 import java.util.*;
 
-    public class Graph {
-        private Map<String, List<String>> cityMap;
-        public Graph() {
-            cityMap = new HashMap<>();
-        }
+public class Graph {
+    private Map<String, List<String>> cityMap;
+
+    public Graph() {
+        cityMap = new HashMap<>();
     }
+    
+}
