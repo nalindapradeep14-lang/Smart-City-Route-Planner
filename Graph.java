ab5619ddb91f2decfc00a55d11f12972e64b1b11
Nalinda <nalindagunasekara42@gmail.com>
2025-10-22 12:23:04 +0530

    Add Initial Graph Class
    Created A new Graph Class to reperesent city connections

---
+++ Graph.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+    public class Graph {
+        private Map<String, List<String>> cityMap;
+        public Graph() {
+            cityMap = new HashMap<>();
+        }
+    }
